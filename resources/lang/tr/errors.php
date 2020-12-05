a07b1340decdfc15b5b7795b76ec9a01c7ce070d
Egemen Berker Kızılcan <egemen@umomega.com>
2020-12-05 18:24:55 +0300

    Added Turkish translations

---
+++ resources/lang/tr/errors.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'page_not_found' => 'Sayfa Bulunamadı',
+    'page_not_found_description' => 'Aradığınız sayfayı bulamadık.',
+
+    'unauthorized' => 'Yasak',
+    'unauthorized_description' => 'Bu eylemi yapmak için gerekli izinlere sahip değilsiniz.',
+
+    'internal' => 'Bir Şey Son Derece Yanlış',
+    'internal_description' => 'Bir mucize bekleyin veya web yöneticileriyle iletişime geçin, sizin seçiminiz...',
+    
+    'go_to_dashboard' => 'Genel Bakış\'a Git'
+];
---
+++ resources/lang/tr/general.php
@@ -0,0 +1,55 @@
+<?php
+
+return [
+    'hello' => 'Merhaba',
+    'whoops' => 'Upss',
+    'regards' => 'Saygılarımızla',
+    'mail_trouble_click_to_view_on_browser' => '":actionText" düğmesine tıklarken sorun yaşıyorsanız, aşağıdaki URL\'yi kopyalayıp web tarayıcınıza yapıştırın: [:actionURL](:actionURL)',
+
+    'yes' => 'Evet',
+    'no' => 'Hayır',
+    'clear' => 'Temizle',
+    'library' => 'Kütüphane',
+    'never' => 'Asla',
+
+    'greeting' => 'Merhaba, :name!',
+
+    'load_more' => 'Daha Fazla Yükle',
+
+    'saved_changes' => 'Değişiklikler başarıyla kaydedildi.',
+	'errors_saving' => 'Kaydederken hatalar oluştu!',
+	'errors_deleting' => 'Silerken hatalar oluştu!',
+
+	'search' => 'Arama',
+	'search_for' => 'Aradığım şey...',
+	'page_ct' => 'Sayfa :current/:total',
+	'bulk_actions' => 'Toplu İşlemler',
+	'filter' => 'Filtre',
+	'options' => 'Seçenekler',
+	'all' => 'Tümü',
+	'cancel' => 'İptal',
+	'danger' => 'Tehlike!',
+	'confirm_delete' => 'Silmek istediğinize emin misiniz?',
+	'no_results_found' => 'Hiçbir sonuç bulunamadı.',
+	'copy' => 'Kopyala',
+
+	'create' => 'Oluştur',
+	'delete' => 'Sil',
+	'add' => 'Ekle',
+
+	'no_item_selected' => 'Hiçbir öğe seçili değil.',
+	'choose_one' => 'Birini seçiniz...',
+
+	'delete_translation' => 'Çeviriyi Sil',
+	'deleted_translation' => 'Çeviri silindi.',
+
+	'locale_en' => 'İngilizce',
+	'locale_tr' => 'Türkçe',
+	'locale_fr' => 'Fransızca',
+	'locale_german' => 'Almanca',
+
+	'details' => 'Detaylar',
+	'done' => 'Bitti',
+	'selected' => 'seçili.',
+	'save_changes' => 'Değişiklikleri Kaydet'
+];
